9ea25b15635f6e1de12bdb36eb697519be54df1e
Ashishkasaudhan <ashish.kasaudhan@devopscaptain.com>
2024-08-27 19:06:01 +0530

    first draft of app runner

--- news.py
+++ news.py
@@ -1,26 +1,20 @@
-import requests
-from bs4 import BeautifulSoup
-from flask import Flask, render_template
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+import os
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    # Fetch the news website
-    url = 'https://news.google.com'
-    response = requests.get(url)
-
-    # Parse the HTML content
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Find the news headlines
-    headlines = soup.find_all('a', class_='DY5T1d')
-
-    # Extract the text from the headlines
-    news_headlines = [headline.text for headline in headlines]
-
-    # Render the template with the news headlines
-    return render_template('index.html', headlines=news_headlines)
+def hello_world(request):
+    name = os.environ.get('NAME')
+    if name == None or len(name) == 0:
+        name = "world"
+    message = "Hello, " + name + "!\n"
+    return Response(message)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    port = int(os.environ.get("PORT"))
+    with Configurator() as config:
+        config.add_route('hello', '/')
+        config.add_view(hello_world, route_name='hello')
+        app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', port, app)
+    server.serve_forever()
